d469604e8830220c3ee012ec939205e0fdbbafba
Alexandre Cassanelli <sacdeneu@gmail.com>
2020-02-01 12:28:48 +0100

    Added ridgid noise filter

--- Assets/Scripts/PlanetScripts/RidgidNoiseFilter.cs
+++ Assets/Scripts/PlanetScripts/RidgidNoiseFilter.cs
@@ -2,12 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NoiseFilter
+public class RidgidNoiseFilter : INoiseFilter
 {
-    NoiseSettings settings;
+    NoiseSettings.RidgidNoiseSettings settings;
     Noise noise = new Noise();
 
-    public NoiseFilter(NoiseSettings settings)
+    public RidgidNoiseFilter(NoiseSettings.RidgidNoiseSettings settings)
     {
         this.settings = settings;
     }
@@ -17,18 +17,20 @@ public class NoiseFilter
         float noiseValue = 0;
         float frequency = settings.baseRoughness;
         float amplitude = 1;
+        float weight = 1;
 
-        for(int i =0; i < settings.numLayers; i++)
+        for (int i = 0; i < settings.numLayers; i++)
         {
-            float v = noise.Evaluate(point * frequency + settings.centre);
-            noiseValue += (v + 1) * .5f * amplitude;
+            float v = 1-Mathf.Abs(noise.Evaluate(point * frequency + settings.centre));
+            v *= v;
+            v *= weight;
+            weight = Mathf.Clamp01(v * settings.weightMultiplier); //on garde entre 0 et 1
+            noiseValue += v * amplitude; //valeur absolue alors déjà entre 0 et 1 
             frequency *= settings.roughness; //roughness > 1 then frequency increase
             amplitude *= settings.persistence; //persistence < 1 then amplitude decrease
         }
         noiseValue = Mathf.Max(0, noiseValue - settings.minValue);
         return noiseValue * settings.strength;
     }
-
-
 
 }
