d54be2f374ce11d0274c7a16f602d377917439c9
Guillaume FONT <33863138+Xgraill@users.noreply.github.com>
2020-02-01 13:20:21 +0100

    try gravity
    and delete spell testing

---
+++ Assets/Scripts/PlanetScripts/Gravity.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// physics to 0 to use.
+[RequireComponent(typeof(SphereCollider))]
+public class Gravity : MonoBehaviour
+{
+    [SerializeField] float forceGravitionelle= 50.0f;// à moduler
+    SphereCollider gravityRegions;
+    SphereCollider planet;
+    float distanceAttraction;
+
+    void Awake(){
+        gravityRegions= GetComponent<SphereCollider>();
+        gravityRegions.isTrigger= true;
+        planet= GetComponentInChildren<SphereCollider>();
+        distanceAttraction= gravityRegions.radius - planet.radius;
+    }
+    // Start is called before the first frame update
+    void onTriggerStay(Collider other){
+        if(other.attachedRigidbody){
+            float intensite= Vector3.Distance(transform.position,other.transform.position)/gravityRegions.radius;
+            other.attachedRigidbody.AddForce((transform.position-other.transform.position)* intensite*forceGravitionelle*Time.deltaTime);
+            
+        }
+    }
+}
