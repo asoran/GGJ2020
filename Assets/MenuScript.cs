189e9592c5c67d0f0e42e138a2365a628b2a8912
Etoile <xanafication@gmail.com>
2020-02-02 15:49:20 +0100

    Add Main Menu

--- Assets/MenuScript.cs
+++ Assets/MenuScript.cs
@@ -5,9 +5,19 @@ using TMPro;
 
 public class MenuScript : MonoBehaviour
 {
+    private void Start()
+    {
+        AudioManager.instance.Play("Odyssey");
+    }
+
     public void Play()
     {
         LevelChanger.instance.gameObject.SetActive(true);
         LevelChanger.instance.FadeToNextLevel();
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
     }
 }
---
+++ Assets/SettingsMenu.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using TMPro;
+
+public class SettingsMenu : MonoBehaviour
+{
+    public AudioMixer audioMixer;
+
+    public TextMeshProUGUI volume_text;
+    public void SetVolume(float volume)
+    {
+        audioMixer.SetFloat("Volume", volume);
+
+        volume_text.text = volume.ToString();
+    }
+}
