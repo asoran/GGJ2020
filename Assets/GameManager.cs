b07943e5af1b7034bdbae5cb0ef3b65e67705ff6
Alexandre Cassanelli <sacdeneu@gmail.com>
2020-02-02 09:16:11 +0100

    Settings and game Manager classes added

---
+++ Assets/GameManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+    private bool isCinematicOpeningEnded;
+
+    private void Start()
+    {
+        Settings.gameManager = this;
+        isCinematicOpeningEnded = false;
+    }
+
+}
