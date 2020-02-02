def19a5d0245bad8999d9b07b06821c2a7d86941
Guillaume FONT <33863138+Xgraill@users.noreply.github.com>
2020-02-02 04:36:40 +0100

    Robin Main Scene edit and fix mesh planet pos

--- Assets/Scripts/CameraRotatePlanet.cs
+++ Assets/Scripts/CameraRotatePlanet.cs
@@ -7,6 +7,8 @@ public class CameraRotatePlanet : MonoBehaviour
     public GameObject planet;
     public float mouseSensibility = 2f;
 
+    public List<GameObject> followingCam;
+
     private Camera camGirl;
     private Vector3 offset;
     private float xRotation = 0f;
@@ -19,18 +21,27 @@ public class CameraRotatePlanet : MonoBehaviour
 
     private void LateUpdate()
     {
-        float mouseX = Input.GetAxis("Mouse X") * mouseSensibility;
-        float mouseY = Input.GetAxis("Mouse Y") * mouseSensibility;
+
+        float mouseX = Input.GetAxis("Mouse X") * Time.deltaTime * mouseSensibility;
+        float mouseY = Input.GetAxis("Mouse Y") * Time.deltaTime * mouseSensibility;
 
         bool isDown = Input.GetMouseButton(0);
 
         if(isDown) {
+
             var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             RaycastHit hit;
             if(Physics.Raycast(ray, out hit)){
+                print("raycast");
                 if(hit.collider.gameObject == planet) {
-                    transform.RotateAround(planet.transform.position, transform.up, mouseX * Time.deltaTime * mouseSensibility);
-                    transform.RotateAround(planet.transform.position, -transform.right, mouseY * Time.deltaTime * mouseSensibility);
+                    print("collider");
+                    transform.RotateAround(planet.transform.position, transform.up, mouseX);
+                    transform.RotateAround(planet.transform.position, -transform.right, mouseY);
+                    //transform.LookAt(planet.transform.position, transform.up);
+                    foreach (GameObject go in followingCam) {
+                        go.transform.RotateAround(planet.transform.position, transform.up, mouseX);
+                        go.transform.RotateAround(planet.transform.position, -transform.right, mouseY);
+                    }
                 }
             }
         }
